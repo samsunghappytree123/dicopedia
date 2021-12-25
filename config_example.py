9d8a8619f967f27995c66ea7068a90ab71364358
samsunghappytree123 <samsunghappytree123@naver.com>
2021-12-25 23:03:20 +0900

    style(config_example): black code style
    black 코드 스타일을 사용하였습니다.

--- config_example.py
+++ config_example.py
@@ -1,4 +1,4 @@
 BOT_TOKEN = "[이 부분을 지우고 토큰을 입력하세요!]"
 LOGGER = __import__("colorlog").getLogger("Bot")
-OWNER_IDS = ['이 부분을 지우고 소유자의 ID를 입력하세요! (LIST)']
+OWNER_IDS = ["이 부분을 지우고 소유자의 ID를 입력하세요! (LIST)"]
 MONGO_URI = "[이 부분을 지우고 몽고DB의 URI를 입력하세요!]"
