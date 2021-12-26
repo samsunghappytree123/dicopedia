aeaeab8e9175a986f3518ba5a1611c62706daec8
Bainble0211 <61723993+Bainble0211@users.noreply.github.com>
2021-12-26 18:14:27 +0900

    feat(cogs.wiki): 검색 기능 추가
    위키 문서 검색 기능을 추가하였습니다.

--- cogs/wiki.py
+++ cogs/wiki.py
@@ -9,17 +9,31 @@ from discord_slash.utils.manage_commands import create_choice, create_option
 from discord_slash.utils.manage_components import (
     create_actionrow,
     create_button,
+    create_select,
+    create_select_option,
     wait_for_component,
 )
 
 from utils.database import USER_DATABASE, WIKI_DATABASE
 from utils.embed import Embed
 
-
 class Wiki(commands.Cog, name="위키"):
     def __init__(self, bot):
         self.bot = bot
         self.logger = bot.logger
+        
+    def cog_check():
+        async def check(ctx):
+            if (await USER_DATABASE.user_find(ctx.author.id)) is None:
+                embed = Embed.warn(
+                    description = "위키를 사용하기 위해서는 가입이 필요합니다.\n`/가입` 명령어를 사용하여 서비스에 가입해주세요.",
+                    timestamp = ctx.created_at
+                )
+                Embed.user_footer(embed, ctx)
+                await ctx.send(embed=embed, hidden=True)
+                return False
+            return True
+        return commands.check(check)
 
     @cog_ext.cog_slash(
         name="생성",
@@ -33,6 +47,7 @@ class Wiki(commands.Cog, name="위키"):
             ),
         ],
     )
+    @cog_check()
     async def doc_create(self, ctx, 제목: str):
         if (await WIKI_DATABASE.wiki_find(제목, ctx.author.id)) != None:
             embed = Embed.warn(
@@ -43,13 +58,18 @@ class Wiki(commands.Cog, name="위키"):
             return await ctx.send(embed=embed, hidden=True)
 
         embed = Embed.warn(description="")
-        m = await ctx.send("문서에 등록할 내용을 입력해주세요.")
+        m = await ctx.send(
+            "문서에 등록할 내용을 입력해주세요."
+            #, components = create_actionrow(create_button(style=ButtonStyle.green, label="취소", custom_id="cancel"))
+        )
 
         def check(content):
             return content.author == ctx.author and content.channel == ctx.channel
 
         try:
             content = await self.bot.wait_for("message", timeout=60, check=check)
+            if content.content == "취소":
+                return await m.edit("사용자에 의해 취소되었습니다.", embed = None)
             result = await WIKI_DATABASE.wiki_create(제목, content.content, ctx.author.id)
             if result["status"] == "success":
                 embed = Embed.default(
@@ -71,7 +91,7 @@ class Wiki(commands.Cog, name="위키"):
         except asyncio.TimeoutError:
             cancel_embed = Embed.warn(
                 description="시간초과로 취소되었어요.",
-                timestamp=content.created_at,
+                timestamp=ctx.created_at,
             )
             Embed.user_footer(cancel_embed, ctx)
             await m.delete()
@@ -104,7 +124,7 @@ class Wiki(commands.Cog, name="위키"):
             Embed.user_footer(embed, ctx)
             return await ctx.send(embed=embed, hidden=True)
 
-        result = await WIKI_DATABASE.wiki_content_find(제목, 편집판, ctx.author.id)
+        result = await WIKI_DATABASE.wiki_content_find(제목, ctx.author.id, 편집판)
 
         if result["status"] == "failed":
             embed = Embed.warn(description=result["content"])
@@ -116,7 +136,22 @@ class Wiki(commands.Cog, name="위키"):
             timestamp=result["content"]["updated_at"] - datetime.timedelta(hours=9),
         )
         embed.set_footer(text=f"작성자 ID : {result['content']['author']}")
-        await ctx.send(embed=embed)
+        
+        buttons = [
+            create_button(
+                style = ButtonStyle.green,
+                label = "수정하기",
+                emoji = "✏️",
+                custom_id = "edit-{}".format(result['title']),
+            ),
+            create_button(
+                style = ButtonStyle.red,
+                label = "신고하기",
+                emoji = "🚨",
+                custom_id = "report-{}".format(result['title']),
+            )
+        ]
+        await ctx.send(embed=embed, components = [create_actionrow(*buttons)])
 
     @cog_ext.cog_slash(
         name="수정",
@@ -130,8 +165,9 @@ class Wiki(commands.Cog, name="위키"):
             ),
         ],
     )
+    @cog_check()
     async def doc_modify(self, ctx, 제목: str):
-        doc = await WIKI_DATABASE.wiki_find(제목, ctx.author.id)
+        doc = await WIKI_DATABASE.wiki_content_find(제목, ctx.author.id)
         if doc == None:
             embed = Embed.warn(
                 description="존재하지 않는 문서에요.\n해당 제목을 가진 문서를 생성하시겠어요?",
@@ -230,10 +266,12 @@ class Wiki(commands.Cog, name="위키"):
                 Embed.user_footer(cancel_embed, ctx)
                 return await target.edit(embed=cancel_embed, components=[])
 
+        doc2 = await WIKI_DATABASE.wiki_content_find(제목, ctx.author.id)
         embed = Embed.default(
-            description=f"`{doc['content']['_id']}` 문서에 넣을 내용을 입력해주세요.",
+            description=f"`{doc2['title']}` 문서에 넣을 내용을 입력해주세요.",
             timestamp=ctx.created_at,
         )
+        embed.add_field(name="현재 내용", value=f"```{doc2['content']['content']}```")
         m = await ctx.send(embed=embed)
 
         def check(content):
@@ -241,6 +279,8 @@ class Wiki(commands.Cog, name="위키"):
 
         try:
             content = await self.bot.wait_for("message", timeout=60, check=check)
+            if content.content == "취소":
+                return await m.edit(content="사용자에 의해 취소되었습니다.", embed = None)
             result = await WIKI_DATABASE.wiki_edit(제목, content.content, ctx.author.id)
             if result["status"] == "success":
                 embed = Embed.default(
@@ -267,6 +307,90 @@ class Wiki(commands.Cog, name="위키"):
             await m.delete()
             return await ctx.send(embed=cancel_embed, hidden=True)
 
+    @cog_ext.cog_slash(
+        name="검색",
+        description="작성된 문서를 검색합니다.",
+        options=[
+            create_option(
+                name="키워드",
+                description="위키에서 검색할 키워드를 입력해주세요.",
+                option_type=3,
+                required=True
+            )
+        ]
+    )
+    async def doc_find(self, ctx, 키워드: str):
+        doc = await WIKI_DATABASE.wiki_search(키워드, ctx.author.id)
+        if len(doc) == 0:
+            return await ctx.send("검색 결과가 없어요.")
+        if len(doc) == 1:
+            result = await WIKI_DATABASE.wiki_content_find(str(doc[0]['_id']), ctx.author.id)
+            m = await ctx.send(embed=Embed.default(title="잠시만 기다려주세요...."))
+        else:
+            embed = discord.Embed(
+                colour=discord.Colour.blurple(),
+                title="🔎 검색 결과",
+                description=f"총 ``{len(doc)}``개의 검색 결과가 있어요. 확인하고 싶은 문서를 선택해주세요!",
+            )
+            listemoji = ["1️⃣", "2️⃣", "3️⃣", "4️⃣", "5️⃣", "6️⃣", "7️⃣", "8️⃣", "9️⃣", "🔟"]
+            count = 0
+            selects = []
+            for i in doc:
+                count += 1
+                if count > 10:
+                    break
+                selects.append(create_select_option(i['_id'], value=str(i['_id']), emoji=listemoji[count-1], default=False))
+            selects.append(create_select_option("취소하기", value="cancel", emoji="🚫", default=False))
+            select = create_select(
+                options=selects,
+                placeholder="확인을 원하는 문서를 선택해주세요.",
+                min_values=0,
+                max_values=1,
+            )
+            action_row = create_actionrow(select)
+            m = await ctx.send(
+                embed=embed,
+                components=[action_row]
+            )
+            def check(res):
+                return (
+                    res.author_id == ctx.author.id
+                    and res.channel == ctx.channel
+                    and res.origin_message_id == m.id
+                )
+
+            try:
+                res: ComponentContext = await wait_for_component(
+                    self.bot, messages=m, check=check, timeout=60.0
+                )
+                if str(res.values[0]) == "cancel":
+                    embed = Embed.warn(description="사용자에 의해 취소되었어요.", timestamp=ctx.created_at)
+                    Embed.user_footer(embed, ctx)
+                    return await m.edit(
+                        embed=embed,
+                        components=[],
+                    )
+            except asyncio.TimeoutError:
+                cancel_embed = Embed.warn(
+                    description="시간초과로 취소되었어요.",
+                    timestamp=ctx.created_at,
+                )
+                Embed.user_footer(cancel_embed, ctx)
+                return await m.edit(embed=cancel_embed, components=[])
+
+            result = await WIKI_DATABASE.wiki_content_find(str(res.values[0]), ctx.author.id)
+
+        if result["status"] == "failed":
+            embed = Embed.warn(description=result["content"])
+            Embed.user_footer(embed, ctx)
+            return await m.edit(embed=embed)
+        embed = Embed.default(
+            title=f"{result['title']} (r{result['r']}판)",
+            description=result["content"]["content"],
+            timestamp=result["content"]["updated_at"] - datetime.timedelta(hours=9),
+        )
+        embed.set_footer(text=f"작성자 ID : {result['content']['author']}")
+        await m.edit(embed=embed, components=[])
 
 def setup(bot):
     bot.add_cog(Wiki(bot))
