3ca0f72d23458f1f11aee1ea7b14730f77edb0d4
samsunghappytree123 <samsunghappytree123@naver.com>
2022-01-22 23:09:08 +0900

    feat(config): add koreanbots token
    koreanbots의 토큰 입력칸을 추가하였습니다.

--- config_example.py
+++ config_example.py
@@ -2,3 +2,4 @@ BOT_TOKEN = "[이 부분을 지우고 토큰을 입력하세요!]"
 LOGGER = __import__("colorlog").getLogger("Bot")
 OWNER_IDS = ["이 부분을 지우고 소유자의 ID를 입력하세요! (LIST)"]
 MONGO_URI = "[이 부분을 지우고 몽고DB의 URI를 입력하세요!]"
+KOREANBOTS_TOKEN = "[이 부분을 지우고 KOREANBOTS의 토큰을 입력해주세요. 없으면 비워주세요.]"
